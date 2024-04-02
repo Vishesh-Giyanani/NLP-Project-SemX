6462fcd9c5472b3bb4cd9bac7d0c05e79454ef7b
Vishesh Giyanani <87827228+Vishesh-Giyanani@users.noreply.github.com>
2024-04-02 23:18:07 +0530

    d

--- tenerflow.py
+++ tenerflow.py
@@ -7,5 +7,4 @@ def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.json()
 
-output = query({"inputs": "The answer to the universe is",})
-print(output)
+query({"inputs": "The answer to the universe is"})
