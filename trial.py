710354d37657269f007b44625576eb5a54bcface
Vishesh Giyanani <87827228+Vishesh-Giyanani@users.noreply.github.com>
2024-04-02 23:00:09 +0530

    lp

---
+++ trial.py
@@ -0,0 +1,11 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/barghavani/English_to_Hindi"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+output = query({"inputs": "The answer to the universe is",})
+print(output)
